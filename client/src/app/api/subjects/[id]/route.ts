5b8143b2f3812026334ad125a22cd076e34ad36a
SyP4lk <53273626+SyP4lk@users.noreply.github.com>
2025-08-07 18:40:15 +0700

    Update route.ts

--- client/src/app/api/subjects/[id]/route.ts
+++ client/src/app/api/subjects/[id]/route.ts
@@ -2,9 +2,17 @@ import { NextRequest, NextResponse } from "next/server";
 
 const API_URL = process.env.NEXT_PUBLIC_API_URL;
 
-export async function DELETE(req: NextRequest, context: any) {
+type Context = {
+  params: {
+    id: string;
+  };
+};
+
+export async function DELETE(req: NextRequest, context: Context) {
   const { id } = context.params;
+
   const res = await fetch(`${API_URL}/${id}`, { method: "DELETE" });
+
   if (!res.ok) {
     return NextResponse.json({ error: "Failed to delete" }, { status: 500 });
   }
@@ -12,14 +20,19 @@ export async function DELETE(req: NextRequest, context: any) {
   return NextResponse.json({ ok: true });
 }
 
-export async function PATCH(req: NextRequest, context: any) {
+export async function PATCH(req: NextRequest, context: Context) {
   const { id } = context.params;
   const body = await req.json();
+
   const res = await fetch(`${API_URL}/${id}`, {
     method: "PATCH",
     headers: { "Content-Type": "application/json" },
     body: JSON.stringify(body),
   });
+
+  if (!res.ok) {
+    return NextResponse.json({ error: "Failed to update" }, { status: 500 });
+  }
 
   return NextResponse.json(await res.json());
 }
